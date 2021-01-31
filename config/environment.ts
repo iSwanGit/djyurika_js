54bce5714542cc25f764614301604326704cef11
Seungwan Goh <emakr518@gmail.com>
2021-01-31 13:24:59 +0900

    Initial commit with examples

---
+++ config/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  prefix: '~',
+  
+  // voiceChannelID: '765822961051369472',
+  voiceChannelID: '775226652430172231',
+	// commandChannelID: '763904888898977832',
+	commandChannelID: '762474528840351774',
+	serverID: '759404553451601920',
+}
---
+++ config/key.ts
@@ -0,0 +1,5 @@
+export const keys = {
+  dbServer: 'db_host_port_',
+  botToken: 'your_token',
+	youtubeApiKey: 'your_key',
+}
