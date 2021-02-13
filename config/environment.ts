b72341c893c1175fc0a0a202b101289911474612
Seungwan Goh <emakr518@gmail.com>
2021-02-13 17:19:03 +0900

    rename and fix envs

--- config/environment.ts
+++ config/environment.ts
@@ -1,10 +1,8 @@
 export const environment = {
   prefix: '~',
   
-  // voiceChannelID: '765822961051369472',
-  voiceChannelID: '775226652430172231',
-	// commandChannelID: '763904888898977832',
-	commandChannelID: '762474528840351774',
+  voiceChannelID: '765822961051369472',
+	commandChannelID: '763904888898977832',
   developerRoleID: '760741584492298251',
 	serverID: '759404553451601920',
 }
