1572c74941d7f95d9c4f123cf1d6fbeba25ae3bd
Seungwan Goh <emakr518@gmail.com>
2021-02-14 01:54:46 +0900

    Change tab to space

--- config/environment.ts
+++ config/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   prefix: '~',
   
   voiceChannelID: '765822961051369472',
-	commandChannelID: '763904888898977832',
+  commandChannelID: '763904888898977832',
   developerRoleID: '760741584492298251',
   moderatorRoleID: '760806219509137418',
 	serverID: '759404553451601920',
