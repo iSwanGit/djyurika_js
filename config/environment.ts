6621330008731af949d2080fbcd241d734d2c11d
Seungwan Goh <emakr518@gmail.com>
2021-03-20 22:48:51 +0900

    Add default override config for debug

--- config/environment.ts
+++ config/environment.ts
@@ -8,4 +8,22 @@ export const environment = {
   youtubeUrlPrefix: 'https://www.youtube.com/watch?v=',
   maxSearchResults: 5,
   refreshInterval: 13000,
+
+  // for debug
+  overrideConfigs: [
+    {
+      serverID: '759404553451601920', // mmk
+      volume: 40,
+      developerRoleID: '760741584492298251',
+      moderatorRoleID: '760806219509137418',
+      commandChannelID: '762474528840351774',  // bot-command
+    },
+    {
+      serverID: '706826945010204743', // mikuwallets
+      volume: 35,
+      developerRoleID: '757615133412425850',  // admin
+      moderatorRoleID: null,
+      commandChannelID: '822370707942014976', // talk
+    },
+  ],
 }
