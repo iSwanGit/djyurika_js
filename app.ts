12508272b0df6a28ca0f7d2170774dabac21e854
Seungwan Goh <emakr518@gmail.com>
2021-05-07 23:04:22 +0900

    Update dependancies

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import consoleStamp from 'console-stamp';
 import { DJYurika } from './djyurika';
 
 consoleStamp(console, {
-  pattern: 'yyyy/mm/dd HH:MM:ss.l',
+  format: ':date(yyyy/mm/dd HH:MM:ss.l)'
 });
 
 process.setMaxListeners(0); // release limit (for voicestatechange event handler)
